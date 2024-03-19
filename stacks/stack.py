3a99157ead537d15e632b7a415ecc4e2e800d237
aqibcs <aqibshahcs@gmail.com>
2024-03-19 15:09:39 +0500

    Stacks

---
+++ stacks/stack.py
@@ -0,0 +1,23 @@
+stack = []
+
+# append() fucntion to push element in the stack
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Initial stack')
+print(stack)
+
+# pop() function to pop element from
+# stack in LIFO order
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped:')
+print(stack)
+
+# print(stack.pop())
+# uncommenting print(stack.pop()) will cause an
+# IndexError as the stack is now empty
